3ee97584e04aca69a20a67dc8a70b60ad6c23d9c
ervsoft <ag81@aof.anadolu.edu.tr>
2021-12-21 21:58:54 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var middleware = require("./middleware");
+var app = express();
+var port = 3000;
+
+
+
+app.use(middleware.logger);
+
+app.get("/hakkimda",middleware.requireAuthentication,function(req,res){
+    res.send("hakkımda sayfası");
+
+})
+
+app.use(express.static(__dirname + '/public'));
+
+//console.log();
+
+app.listen(port,function(){
+    console.log("Express server  " + port + " nolu porttan  başlatıldı");
+});
+    
